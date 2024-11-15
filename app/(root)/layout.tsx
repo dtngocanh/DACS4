79d16eb2944f1e2e5e010ffb2ab6b0291703676d
anhngocthiduong <anh.dtng@gmail.com>
2024-11-15 08:49:00 +0700

    step 5

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,11 @@
+import StreamVideoProvider from '@/providers/StreamClientProvider'
 import React, { ReactNode } from 'react'
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main>
-      {children}
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+
     </main>
   )
 }
